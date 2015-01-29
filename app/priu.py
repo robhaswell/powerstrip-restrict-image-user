eff976034df3f3c076d4088f97982160745415ed
Rob <me@robhaswell.co.uk>
2015-01-29 17:37:55 +0000

    Tests for official images.

--- app/priu.py
+++ app/priu.py
@@ -30,6 +30,11 @@ if __name__ == "__main__":
     try:
         app.config['ALLOWED_USER'] = os.environ['USER']
     except KeyError:
-        sys.stdout.write("Error: Configuration environment variable USER not provided. Specify an image username on the Docker command-line by using docker run -e USER=<user>\n")
+        sys.stdout.write("""Error: Configuration environment variable USER not provided.
+
+Specify an image username on the Docker command-line by using docker run -e USER=<user>.
+
+Use the user "_" to only allow official Docker images.
+""")
         sys.exit(1)
     app.run(port=80)
--- app/test_priu.py
+++ app/test_priu.py
@@ -9,12 +9,12 @@ class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = priu.app.test_client()
-        priu.app.config['ALLOWED_USER'] = "good"
 
     def test_integration_create_user_success(self):
         """
         The request is returned unmodified when the user is allowed.
         """
+        priu.app.config['ALLOWED_USER'] = "good"
         path = "/v1.16/containers/create"
         json = r'{"Hostname":"","Domainname":"","User":"","Memory":0,"MemorySwap":0,"CpuShares":0,"Cpuset":"","AttachStdin":false,"AttachStdout":true,"AttachStderr":true,"PortSpecs":null,"ExposedPorts":{},"Tty":false,"OpenStdin":false,"StdinOnce":false,"Env":[],"Cmd":null,"Image":"good/container","Volumes":{},"WorkingDir":"","Entrypoint":null,"NetworkDisabled":false,"MacAddress":"","OnBuild":null,"HostConfig":{"Binds":null,"ContainerIDFile":"","LxcConf":[],"Privileged":false,"PortBindings":{},"Links":null,"PublishAllPorts":false,"Dns":null,"DnsSearch":null,"ExtraHosts":null,"VolumesFrom":null,"Devices":[],"NetworkMode":"bridge","IpcMode":"","CapAdd":null,"CapDrop":null,"RestartPolicy":{"Name":"","MaximumRetryCount":0},"SecurityOpt":null}}'
         request_json = self._make_powerstrip_pre_hook_request("POST", path, json)
@@ -31,6 +31,39 @@ class FlaskrTestCase(unittest.TestCase):
         """
         The status code is 403 when the user is bad.
         """
+        priu.app.config['ALLOWED_USER'] = "good"
+        path = "/v1.16/containers/create"
+        json = r'{"Hostname":"","Domainname":"","User":"","Memory":0,"MemorySwap":0,"CpuShares":0,"Cpuset":"","AttachStdin":false,"AttachStdout":true,"AttachStderr":true,"PortSpecs":null,"ExposedPorts":{},"Tty":false,"OpenStdin":false,"StdinOnce":false,"Env":[],"Cmd":null,"Image":"bad/container","Volumes":{},"WorkingDir":"","Entrypoint":null,"NetworkDisabled":false,"MacAddress":"","OnBuild":null,"HostConfig":{"Binds":null,"ContainerIDFile":"","LxcConf":[],"Privileged":false,"PortBindings":{},"Links":null,"PublishAllPorts":false,"Dns":null,"DnsSearch":null,"ExtraHosts":null,"VolumesFrom":null,"Devices":[],"NetworkMode":"bridge","IpcMode":"","CapAdd":null,"CapDrop":null,"RestartPolicy":{"Name":"","MaximumRetryCount":0},"SecurityOpt":null}}'
+        request_json = self._make_powerstrip_pre_hook_request("POST", path, json)
+
+        rv = self.app.post("/", data=request_json, headers={
+            "content-type": "application/json"})
+
+        self.assertEquals(rv.status_code, 403)
+
+    def test_integration_create_official_success(self):
+        """
+        The request is returned unmodified when the official is allowed.
+        """
+        priu.app.config['ALLOWED_USER'] = "_"
+        path = "/v1.16/containers/create"
+        json = r'{"Hostname":"","Domainname":"","User":"","Memory":0,"MemorySwap":0,"CpuShares":0,"Cpuset":"","AttachStdin":false,"AttachStdout":true,"AttachStderr":true,"PortSpecs":null,"ExposedPorts":{},"Tty":false,"OpenStdin":false,"StdinOnce":false,"Env":[],"Cmd":null,"Image":"container","Volumes":{},"WorkingDir":"","Entrypoint":null,"NetworkDisabled":false,"MacAddress":"","OnBuild":null,"HostConfig":{"Binds":null,"ContainerIDFile":"","LxcConf":[],"Privileged":false,"PortBindings":{},"Links":null,"PublishAllPorts":false,"Dns":null,"DnsSearch":null,"ExtraHosts":null,"VolumesFrom":null,"Devices":[],"NetworkMode":"bridge","IpcMode":"","CapAdd":null,"CapDrop":null,"RestartPolicy":{"Name":"","MaximumRetryCount":0},"SecurityOpt":null}}'
+        request_json = self._make_powerstrip_pre_hook_request("POST", path, json)
+        expected_json = lib.pre_hook_response("POST", path, json)
+
+        rv = self.app.post("/", data=request_json, headers={
+            "content-type": "application/json"})
+
+        self.assertEquals(rv.status_code, 200)
+        self.assertEquals(rv.data, expected_json)
+        self.assertEquals(rv.headers['content-type'], "application/json")
+
+    def test_integration_create_office_fail(self):
+        """
+        The status code is 403 when the user and only official Docker images
+        are allowed.
+        """
+        priu.app.config['ALLOWED_USER'] = "_"
         path = "/v1.16/containers/create"
         json = r'{"Hostname":"","Domainname":"","User":"","Memory":0,"MemorySwap":0,"CpuShares":0,"Cpuset":"","AttachStdin":false,"AttachStdout":true,"AttachStderr":true,"PortSpecs":null,"ExposedPorts":{},"Tty":false,"OpenStdin":false,"StdinOnce":false,"Env":[],"Cmd":null,"Image":"bad/container","Volumes":{},"WorkingDir":"","Entrypoint":null,"NetworkDisabled":false,"MacAddress":"","OnBuild":null,"HostConfig":{"Binds":null,"ContainerIDFile":"","LxcConf":[],"Privileged":false,"PortBindings":{},"Links":null,"PublishAllPorts":false,"Dns":null,"DnsSearch":null,"ExtraHosts":null,"VolumesFrom":null,"Devices":[],"NetworkMode":"bridge","IpcMode":"","CapAdd":null,"CapDrop":null,"RestartPolicy":{"Name":"","MaximumRetryCount":0},"SecurityOpt":null}}'
         request_json = self._make_powerstrip_pre_hook_request("POST", path, json)
